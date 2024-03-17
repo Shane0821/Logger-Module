19bcc2280abb21c79115426910be99e46b59ac0f
Shane <143796207+Shane0821@users.noreply.github.com>
2024-03-17 17:24:37 +0800

    add test

---
+++ test/test.py
@@ -0,0 +1,72 @@
+import unittest
+ 
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname( __file__ ), '..', 'component'))
+from ILog import LogComponent
+
+import time
+
+class TestLogComponent(unittest.TestCase):
+    def test_write(self):
+        logger = LogComponent()
+        logger.clear_logs()
+
+        ts1 = logger.write("This is test message 1.")
+        time.sleep(5)
+        ts2 = logger.write("This is test message 2.")
+        logger.stop()
+
+        expected = f"{ts1.strftime('%H:%M:%S')}: This is test message 1.\n{ts2.strftime('%H:%M:%S')}: This is test message 2.\n"
+
+        with open(f"{logger.path}/log_{ts1.strftime('%Y_%m_%d')}.txt", 'r') as file:
+            actual = file.read()
+        self.assertEqual(expected, actual)
+
+    def test_stop_with_delay(self):
+        logger = LogComponent()
+        logger.clear_logs()
+
+        ts1 = logger.write("This is test message 1.")
+        time.sleep(5)
+        ts2 = logger.write("This is test message 2.")
+        logger.stop()
+        logger.write("This is test message 3.")
+        logger.write("This is test message 4.")
+
+        expected = f"{ts1.strftime('%H:%M:%S')}: This is test message 1.\n{ts2.strftime('%H:%M:%S')}: This is test message 2.\n"
+
+        with open(f"{logger.path}/log_{ts1.strftime('%Y_%m_%d')}.txt", 'r') as file:
+            actual = file.read()
+        self.assertEqual(expected, actual)
+    
+    def test_stop_without_delay(self):
+        logger = LogComponent()
+        logger.clear_logs()
+
+        ts1 = logger.write("This is test message 1.")
+        time.sleep(5)
+        ts2 = logger.write("This is test message 2.")
+        logger.stop(False)
+        logger.write("This is test message 3.")
+        logger.write("This is test message 4.")
+
+        expected = f"{ts1.strftime('%H:%M:%S')}: This is test message 1.\n{ts2.strftime('%H:%M:%S')}: This is test message 2.\n"
+
+        with open(f"{logger.path}/log_{ts1.strftime('%Y_%m_%d')}.txt", 'r') as file:
+            actual = file.read()
+        self.assertEqual(expected, actual)
+
+    def test_cross_date(self):
+        logger = LogComponent()
+        logger.clear_logs()
+
+        ts = logger.write("This is test message 1.")
+
+        logger.stop()
+
+        self.assertTrue(os.path.exists(f"{logger.path}/log_{ts.strftime('%Y_%m_%d')}.txt"))
+
+ 
+if __name__ == '__main__':
+    unittest.main()
